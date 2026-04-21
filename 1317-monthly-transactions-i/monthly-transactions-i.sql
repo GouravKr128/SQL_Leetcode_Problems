61769d6336732058ecf85dc56a9eb221d5f3f36e
Gourav Kumar <gk128128@gmail.com>
2026-04-22 01:45:25 +0530

    Time: 627 ms (24.88%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+select date_format(trans_date,'%Y-%m') as month, country, count(*) as trans_count, 
+sum(case
+    when state='approved' then 1
+    else 0
+end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case
+        when state='declined' then 0
+        else amount
+        end) as approved_total_amount
+from transactions
+group by month,country
