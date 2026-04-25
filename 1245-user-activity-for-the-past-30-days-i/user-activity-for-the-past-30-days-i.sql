663f245cd0e330a57a1e2ff4bd5347903f03eb11
Gourav Kumar <gk128128@gmail.com>
2026-04-26 04:47:07 +0530

    Time: 516 ms (27.21%) | Memory: 0B (100.00%) - LeetSync

--- 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
+++ 1245-user-activity-for-the-past-30-days-i/user-activity-for-the-past-30-days-i.sql
@@ -6,5 +6,5 @@ with tbl as(
 
 select activity_date as day, count(distinct user_id) as active_users
 from tbl
-where datediff('2019-07-27',activity_date)<30 and datediff('2019-07-27',activity_date)>=0
+where datediff('2019-07-27',activity_date)<30 and activity_date<='2019-07-27'
 group by activity_date
