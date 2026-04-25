93a70f6685aae4d73ea0b27fcee9551c911eb5c0
Gourav Kumar <gk128128@gmail.com>
2026-04-25 23:59:39 +0530

    Time: 532 ms (9.96%) | Memory: 0B (100.00%) - LeetSync

---
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+with tbl as(
+select num
+from mynumbers
+group by num
+having count(num) = 1)
+
+select 
+case 
+    when count(*) = 0 then null
+    else max(num)
+end as num
+from tbl
