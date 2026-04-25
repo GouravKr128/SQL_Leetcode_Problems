1e54b26dcba2330ff3531e4863b2628a8f6fca9d
Gourav Kumar <gk128128@gmail.com>
2026-04-26 00:52:27 +0530

    Time: 315 ms (13.14%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,4 +1,9 @@
 # Write your MySQL query statement below
+ select max(salary) as SecondHighestSalary
+ from employee 
+ where salary<(select max(salary) from employee)
+
+/* 
 with tbl as(
 select id,salary,
 dense_rank() over(order by salary desc) as rnk
@@ -8,3 +13,4 @@ select distinct
 case when count(*) =0 then null else salary end as SecondHighestSalary
 from tbl
 where rnk=2
+*/
