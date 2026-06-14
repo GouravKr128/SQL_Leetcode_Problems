73ca6ea942b94ed2ce0200dfdf089b76acf7d407
Gourav Kumar <gk128128@gmail.com>
2026-06-14 21:17:21 +0530

    Time: 1523 ms (61.68%) | Memory: 0B (100.00%) - LeetSync

--- 2057-count-salary-categories/count-salary-categories.sql
+++ 2057-count-salary-categories/count-salary-categories.sql
@@ -1,4 +1,28 @@
 # Write your MySQL query statement below
+# Method 1 
+SELECT 'Low Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income < 20000
+
+UNION
+
+SELECT 'Average Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income BETWEEN 20000 AND 50000
+
+UNION
+
+SELECT 'High Salary' AS category,
+       COUNT(*) AS accounts_count
+FROM Accounts
+WHERE income > 50000;
+
+
+
+/* Method 2
+
 with tbl1 as(
 select *, case 
     when income<20000 then 'Low Salary'
@@ -21,3 +45,5 @@ group by category)
 union
 (select * from tbl2
 where category not in (select distinct category from tbl1) )
+
+*/
